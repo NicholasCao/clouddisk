dc772c798675c7da030c73d15c20be3714fd0fc6
Nicholas <982912719@qq.com>
2019-04-20 22:13:51 +0800

    update app.js

--- app.js
+++ app.js
@@ -169,4 +169,4 @@ const server = http.createServer((req, res) => {
 
 server.listen(PORT)
 
-console.log(`Server is running at http://localhost:${PORT}/`)
+console.log(`Server is running at http://localhost:${PORT}`)
